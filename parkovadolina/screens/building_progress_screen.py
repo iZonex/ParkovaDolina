0724c88d85f79cdb3bc4af426db084267e6989a1
Dmytro Chystiakov <dlchistyakov@gmail.com>
2020-07-22 10:04:24 +0300

    add: states for building

--- parkovadolina/screens/building_progress_screen.py
+++ parkovadolina/screens/building_progress_screen.py
@@ -33,6 +33,16 @@ class BuildingScreen:
 
     PREFIX = "Йде етап будівництва: "
 
+    STATUS_MAP = {
+        "1": "✅Будуватися за планом",
+        "2": "❌Будівництво зупинено",
+    }
+
+    STATUS_MAP_SMALL = {
+        "1": "✅",
+        "2": "❌",
+    }
+
     def __init__(self, bot, dao):
         self.bot = bot
         self.dao = dao
@@ -41,14 +51,18 @@ class BuildingScreen:
     def _build_sections(self):
         return [types.KeyboardButton(i) for i in self.SECTIONS]
 
+    def generate_states(self, states_list_ids):
+        return "\n".join([f'- {self.STATUS_MAP_SMALL.get("2")} {self.BUILDING_STATUS_MAP.get(i)}' for i in states_list_ids])
+
     def screen(self, message):
         plans = self.dao.building_plan.get()
         for i in plans.values():
-            text_states = "\n- ".join(list(map(lambda x: self.BUILDING_STATUS_MAP.get(x), i.get_expected_state())))
+            text_states = self.generate_states(i.get_expected_state())
             text_body = (
-                f'<strong>{self.BUILDING_NUMBERS.get(i.title, "").upper()}</strong>\n'
-                f"\n- {text_states}\n\n"
-                f"<strong>Будівництво за планом на {i.get_date()}</strong>"
+                f'<strong>{self.BUILDING_NUMBERS.get(i.title, "").upper()}</strong>\n\n'
+                f'<strong>План будівництва на {i.get_date()}:</strong>\n'
+                f"{text_states}\n\n"
+                f'<strong>Статус будівництва: {self.STATUS_MAP["2"]}</strong>'
             )
             self.bot.send_message(message.chat.id, text_body, parse_mode='HTML')
         keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=1)
