ffa486ae69720230132ec17f7a39a1536535d55d
Dmytro Chystiakov <dlchistyakov@gmail.com>
2020-07-25 13:43:01 +0300

    add: voice chat

--- parkovadolina/screens/communication/router.py
+++ parkovadolina/screens/communication/router.py
@@ -2,12 +2,14 @@ from core.router import Router
 from .main_screen import CommunicationScreen
 from .chats_list_screen import ChatsListScreen
 from .forum_screen import ForumScreen
+from .voice_chats_screen import VoiceChatsScreen
 
 class CommunicationRouter(Router):
 
     routes = (
         ChatsListScreen,
         ForumScreen,
+        VoiceChatsScreen,
     )
 
     default_route = CommunicationScreen
---
+++ parkovadolina/screens/communication/voice_chats_screen.py
@@ -0,0 +1,25 @@
+from telebot import types
+from core.constants import EXIT
+
+class VoiceChatsScreen:
+
+    SECTIONS = [EXIT]
+    LINK = "https://discord.gg/XrTMwWz"
+
+    def __init__(self, bot, dao):
+        self.bot = bot
+        self.dao = dao
+        self.sections = self._build_sections()
+
+    def _build_sections(self):
+        return [types.KeyboardButton(i) for i in self.SECTIONS]
+
+    def screen(self, message):
+        keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=1)
+        keyboard.add(*self.sections)
+        text_body = f"Посилання на голосовий чат:\n{self.FORUM_LINK}"
+        self.bot.send_message(message.chat.id, text_body, reply_markup=keyboard)
+
+    @staticmethod
+    def match(message):
+        return message.text.startswith("Голосовий чат") or False
