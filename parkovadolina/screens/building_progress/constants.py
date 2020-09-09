75488e063311aeb6d30db2aa625ee5237744fb23
Dmytro Chystiakov <dlchistyakov@gmail.com>
2020-09-09 12:06:44 +0300

    add: better photo report user expirience

--- parkovadolina/screens/building_progress/constants.py
+++ parkovadolina/screens/building_progress/constants.py
@@ -25,3 +25,16 @@ STATUS_MAP_SMALL = {
     "3": "❗️",
     "inprogress": "▶️",
 }
+
+PHOTO_ANGLE = {
+    "1.1": "https://parkova-dolyna.com/t/dom-1-rakurs-1-obnovleno-27-08/12",
+    "1.2": "https://parkova-dolyna.com/t/dom-1-rakurs-2-obnovleno-27-08/13/4",
+    "1.3": "https://parkova-dolyna.com/t/dom-1-rakurs-3-obnovleno-27-08/88/2",
+    "1.4": "https://parkova-dolyna.com/t/dom-1-rakurs-4-obnovleno-27-08/14/4",
+    "1.5": "https://parkova-dolyna.com/t/dom-1-rakurs-5-obnovleno-27-08/15/4",
+    "1.6": "https://parkova-dolyna.com/t/dom-1-rakurs-6-obnovleno-27-08/89/2",
+    "2.1": "https://parkova-dolyna.com/t/dom-2-rakurs-1-obnovleno-27-08/90/2",
+}
+
+
+PHOTO_ANGLE_CACHE = [f'<a href="{v}">{k}</a>\n\n' for k,v in PHOTO_ANGLE.items()]
--- parkovadolina/screens/building_progress/main_screen.py
+++ parkovadolina/screens/building_progress/main_screen.py
@@ -1,9 +1,9 @@
+from parkovadolina.screens.building_progress.constants import PHOTO_ANGLE, PHOTO_ANGLE_CACHE
 from parkovadolina.models.building_constants import BUILDING_NUMBERS, STATUS_MAP
 from parkovadolina.core.screen import Screen
 from aiogram import types
 from aiogram.types.message import ParseMode
 from parkovadolina.core.constants import EXIT
-# get_actual_state_of_month == choiced_state. if less then actual state. Issues with building, If equail Normal, If more great
 
 class BuildingMainScreen(Screen):
 
@@ -19,6 +19,12 @@ class BuildingMainScreen(Screen):
         BUILDING_SECTIONS = [i.title for i in plans.values()]
         return [types.KeyboardButton(i) for i in BUILDING_SECTIONS + self.SECTIONS]
 
+    def _build_navigation_buttons(self):
+        keyboard_markup = types.InlineKeyboardMarkup(row_width=2)
+        row_btns = [types.InlineKeyboardButton(k, url=v) for k,v in PHOTO_ANGLE.items()]
+        keyboard_markup.row(*row_btns)
+        return keyboard_markup
+
     async def screen(self, message):
         keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         keyboard.add(*self.sections)
@@ -30,6 +36,11 @@ class BuildingMainScreen(Screen):
             f"{text_result}"
         )
         await self.bot.send_message(message.chat.id, text_template, reply_markup=keyboard, parse_mode=ParseMode.HTML)
+        keyboard_markup = self._build_navigation_buttons()
+        text_template = (
+            f"Фото звіти по будівництву:"
+        )
+        await self.bot.send_message(message.chat.id, text_template, reply_markup=keyboard_markup, parse_mode=ParseMode.HTML)
 
     def match_context(self, message):
         return message.text.startswith("↩️Назад")
